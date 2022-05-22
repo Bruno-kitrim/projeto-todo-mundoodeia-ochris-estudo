a3133452646309c7f61c606878e12838dd959439
Kitrim <101733555+Kitrim@users.noreply.github.com>
2022-05-22 20:35:21 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+// trocar player selecionado
+const personagens = document.querySelectorAll(".personagem");
+
+personagens.forEach((personagem) => {
+  personagem.addEventListener("mouseenter", () => {
+    const idSelecionado = personagem.attributes.id.value;
+
+    if (idSelecionado === "caruso") {
+      return;
+    }
+    const personagemSelecionado = document.querySelector(".selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+
+    personagem.classList.add("selecionado");
+
+    // trocar imagem e nome
+
+    const imageJogador1 = document.getElementById("personagem-jogador-1");
+    imageJogador1.src = `./src/imagens/${idSelecionado}.png`;
+
+    const nomeJogador1 = document.getElementById("nome-jogador-1");
+    const nomeSelecionado = personagem.getAttribute("data-name");
+
+    nomeJogador1.innerHTML = nomeSelecionado;
+  });
+});
